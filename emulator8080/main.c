296d23570d126f10cfcc568fadea80af1a338b62
Paweł Mazurkiewicz <pawelmazurkiewicz@icloud.com>
2016-07-02 09:24:53 +0200

    Initial Commit

---
+++ emulator8080/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  emulator8080
+//
+//  Created by Paweł Mazurkiewicz on 02/07/16.
+//  Copyright © 2016 Paweł Mazurkiewicz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
