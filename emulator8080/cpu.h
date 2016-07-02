2cc6f6de9057945fb4667a97acf725d0de18d95e
Paweł Mazurkiewicz <pawelmazurkiewicz@icloud.com>
2016-07-02 21:42:37 +0200

    The journey begins.

---
+++ emulator8080/cpu.h
@@ -0,0 +1,15 @@
+//
+//  cpu.h
+//  emulator8080
+//
+//  Created by Paweł Mazurkiewicz on 02/07/16.
+//  Copyright © 2016 Paweł Mazurkiewicz. All rights reserved.
+//
+
+#ifndef cpu_h
+#define cpu_h
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#endif /* cpu_h */
